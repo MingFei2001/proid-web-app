93296901ae4dbf0e95ff3c6565b5a53b60eae365
MingFei2001 <mingfei2001@gmail.com>
2024-07-11 13:12:03 +0800

    removed unnecessary routes

--- app.py
+++ app.py
@@ -21,9 +21,5 @@ def index():
     conn.close()
     return render_template('index.html',events=events)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
